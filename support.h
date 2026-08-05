353dede328ec859a071c735287b207cf19497376
frannyJO <144179558+frannyJO@users.noreply.github.com>
2026-08-04 20:46:50 -0500

    Update support.h

--- support.h
+++ support.h
@@ -1,1 +1,14 @@
+#ifndef __FILEH__
+#define __FILEH__
 
+#include <sys/time.h>
+#include "pv_model.h"
+
+typedef struct {
+    struct timeval startTime;
+    struct timeval endTime;
+} Timer;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
