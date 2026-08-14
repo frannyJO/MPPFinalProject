46a80f0ba2374ae8cf23568efd500ded15733042
frannyJO <144179558+frannyJO@users.noreply.github.com>
2026-08-13 21:43:11 -0500

    Update panel_data.h

--- panel_data.h
+++ panel_data.h
@@ -1,1 +1,17 @@
+#ifndef __PANEL_DATAH__
+#define __PANEL_DATAH__
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void generate_plant_conditions(int num_panels,
+                                unsigned int seed,
+                                double *irradiance,
+                                double *temperature);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
